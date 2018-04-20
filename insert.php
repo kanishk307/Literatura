dca0952564cd1f1daad19abf2e617b97ced630d6
kanishk307 <ysj30797.kj@gmail.com>
2018-04-20 21:05:56 +0530

    php

---
+++ insert.php
@@ -0,0 +1,23 @@
+<?php 
+$con = mysqli_connect('localhost:3306','root','root'); 
+
+if(!$con)
+{
+    echo 'Not Connected To Server'; 
+          }
+
+if (!mysqli_select_db ($con,'email_addresses')) 
+{ 
+    echo 'Database Not Selected';
+} 
+
+$Name = $_GET('name');
+$Email = $_GET('email');
+
+$sql = "INSERT INTO info (Name,Email) values ('$Name','$Email')"; 
+if (!mysqli_query($con,$sql))
+{ 
+    echo 'Not Inserted'; 
+} 
+else { echo 'Inserted Successfully'; } header("refresh:2; url=index.html"); 
+?>
